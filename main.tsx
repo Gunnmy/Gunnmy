28cc4f8da0431c63d60fceedaf190d6761e597c5
Gunnmy <102844292+Gunnmy@users.noreply.github.com>
2022-04-02 00:13:01 -0400

    Create main.tsx

---
+++ main.tsx
@@ -0,0 +1,17 @@
+import "./main.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                AAAAAAAAAAAAAAAAAA
+            </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+            </p>
+        </div>
+    );
+}
+
+export default App;
